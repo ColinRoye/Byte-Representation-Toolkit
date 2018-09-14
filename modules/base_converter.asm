90f303acbd3f72e8a70bdc6bb1bcfda1e4c4ae3c
ColinRoye <croye97@gmail.com>
2018-09-14 14:53:40 -0400

    approach to part 4 determined, functions outlined

--- modules/base_converter.asm
+++ modules/base_converter.asm
@@ -8,18 +8,48 @@ base_converter:
     syscall
     move $t0, $a0
 
-    
+    move $a0, $a1
     li $v0, 84
     syscall
     move $t1, $a1
 
-
+    move $a0, $a2
     li $v0, 84
     syscall
     move $t2, $a2
 
+    # $a0 number in a base
+    # $a1 base we are converting from
+    # $a2 base we are converting to
+    move $a0, $t0
+    move $a1, $t1
+    move $a2, $t2
+    
+    bgt $t1, $t2, lower_cvt
 
-    div $v0, $v0, $a1
+    addiu $sp, $sp, -4
+    sw $ra, 0($sp)
 
+
+
+
+
+
+
+
+
+    return_bc:
+
+    #print value
+
+    jr $ra
     #32 bit remainder in HI
     #32 bit remainder in LOW
+
+    base_converter_to_higher_base:
+    #mult, add inc power
+    jr $ra
+
+    base_converter_to_lower_base:
+    #add shift, add shift
+    jr $ra
