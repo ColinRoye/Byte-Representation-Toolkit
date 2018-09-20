17e6d2cb5b43d8deea976e772965864aefd0742d
AzureAD\ColinRoye <croye97@gmail.com>
2018-09-20 00:32:24 -0400

    changes made to expected_p2

--- tests/modules/expected_p2.js
+++ tests/modules/expected_p2.js
@@ -1,16 +1,44 @@
 const invalid_args = 'INVALID_ARGS'
 function expect(a1) {
-    if(a1.length >= 32){
+    if(a1.length > 32){
+        console.log(a1.length)
         return invalid_args
     }
-    let checkInvChar = a1.replace('1', '');
-    checkInvChar = a1.replace('0', '');
+    let sign = a1.charAt(0);
+    if(sign == "1" ){
+        a1 = complement(a1)
+    }
+    let checkInvChar = a1;
+        checkInvChar = checkInvChar.replace(/0|1/g, '');
 
     if(checkInvChar.length  !== 0){
         return invalid_args
     }
-
-    return ~~parseInt(a1,2)
+    if(sign == "0"){
+        return ~~parseInt(a1,2)
+    }
+    return ~parseInt(a1,2)
 }
-module.exports.expect_p2 = expect
+
+function complement(a1) {
+    let str = '';
+    for(let i = 0; i < a1.length; i++) {
+
+        if (a1.charAt(i) == "0") {
+                str += "1"
+            }
+            else if (a1.charAt(i) == "1") {
+                str += "0"
+            }else{str += a1.charAt(i)}
+        }
+        return str
+    }
+
+console.log(expect("10000000000000000000000000000000"));
+
+module.exports.expect_p2 = expect;
+
+
+
+
 
